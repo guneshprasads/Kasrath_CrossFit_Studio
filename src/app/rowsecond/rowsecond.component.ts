9f66f1eba3213dfa279526ae4a7a47b36af15f3a
guneshprasads <guneshprasads2506@gmail.com>
2023-10-17 22:53:17 +0530

    Row Seond

---
+++ src/app/rowsecond/rowsecond.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rowsecond',
+  templateUrl: './rowsecond.component.html',
+  styleUrls: ['./rowsecond.component.css']
+})
+export class RowsecondComponent {
+
+}
