3c76a9aadf8a50c2ba63e76ddc34b5d90a9d6abb
guneshprasads <guneshprasads2506@gmail.com>
2023-10-19 00:55:01 +0530

    rowfive

---
+++ src/app/rowfive/rowfive.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rowfive',
+  templateUrl: './rowfive.component.html',
+  styleUrls: ['./rowfive.component.css']
+})
+export class RowfiveComponent {
+
+}
